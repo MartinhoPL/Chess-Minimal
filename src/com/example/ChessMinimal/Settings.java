45cbeba42ad2ac672b7c423b9608aeae5b104213
renegatp <renegatp@wp.pl>
2015-12-14 01:56:18 +0100

    Poprawa wykrywania mata.

--- src/com/example/ChessMinimal/Settings.java
+++ src/com/example/ChessMinimal/Settings.java
@@ -5,7 +5,7 @@ package com.example.ChessMinimal;
  */
 public final class Settings {
     public static int Time = 10;
-    public static int Mode = 2; //0 - Humans, 1 - HumanWhite. 2 - HumanBlack
+    public static int Mode = 1; //0 - Humans, 1 - HumanWhite. 2 - HumanBlack
     public static boolean ShowEvaluation = false;
     public static String TreeDepth = "5";
 }
